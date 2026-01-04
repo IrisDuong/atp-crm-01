8cc41ad94d8a054bb371b6d2eb5dcf11d476db70
Manhnk2 <manhtt09@gmail.com>
2026-01-04 20:26:51 +0700

    thay doi cach tra ve response

--- atp-crm-be-01/setting/src/main/java/com/atp/crm01/setting/baseData/controller/GeneralCodeController.java
+++ atp-crm-be-01/setting/src/main/java/com/atp/crm01/setting/baseData/controller/GeneralCodeController.java
@@ -15,6 +15,7 @@ import com.atp.crm01.common.exception.BadRequestException;
 import com.atp.crm01.common.utils.ApiUtils;
 import com.atp.crm01.common.utils.CommonUtils;
 import com.atp.crm01.setting.baseData.dto.GeneralCodeRequestDTO;
+import com.atp.crm01.setting.baseData.dto.GeneralCodeResponseDTO;
 import com.atp.crm01.setting.baseData.service.GeneralCodeService;
 
 import lombok.RequiredArgsConstructor;
@@ -28,44 +29,31 @@ public class GeneralCodeController {
 	private final GeneralCodeService generalCodeService;
 	
 	@PostMapping("/create")
-	public ResponseEntity<ApiResponse<?>> createCommonCode(@RequestBody GeneralCodeRequestDTO reqParams){
+	public ResponseEntity<ApiResponse<Boolean>> createCommonCode(@RequestBody GeneralCodeRequestDTO reqParams) throws Exception{
 		if(CommonUtils.isEmptyData(reqParams))
 			throw new BadRequestException("Params is invalid");
 		
 		var result = generalCodeService.createGeneralCode(reqParams);
-		
-		ApiResponse<?> dataResponse;
-		HttpStatus httpStatus;
-		if(result) {
-			httpStatus = HttpStatus.CREATED;
-			dataResponse = ApiUtils.buildApiResponse(null, httpStatus, "Create list general code successfully !") ;
-		}else {
-			httpStatus = HttpStatus.INTERNAL_SERVER_ERROR;
-			dataResponse = ApiUtils.buildApiResponse(null, httpStatus, "Create list general code failed !") ;
-		}
-		log.debug("[GENERAL-CODE-CONTROLLER] - createCommonCode :: {} !",dataResponse.message());
-		return new ResponseEntity<>(dataResponse, httpStatus);
+		ResponseEntity<ApiResponse<Boolean>> dataResponse =  ApiUtils.buildAPIResponse(result, HttpStatus.CREATED, "Create list general code successfully");
+		log.debug("[GENERAL-CODE-CONTROLLER] - createCommonCode :: {} !",dataResponse.getBody().message());
+		return dataResponse;
 	}
 	
 	@PostMapping("/list-by-common-code")
-	public ResponseEntity<ApiResponse<?>> getListGeneralByCommonCode(@RequestBody GeneralCodeRequestDTO param){
+	public ResponseEntity<ApiResponse<List<GeneralCodeResponseDTO>>> getListGeneralByCommonCode(@RequestBody GeneralCodeRequestDTO param){
 		if(CommonUtils.isEmptyData(param))
 			throw new BadRequestException("Params is invalid");
 		
 		var result = generalCodeService.getListGeneralByCommonCode(param);
-		
-		ApiResponse<?> dataResponse;
-		HttpStatus httpStatus;
-		if(!CommonUtils.isEmptyData(result)) {
-			httpStatus = HttpStatus.OK;
-			dataResponse = ApiUtils.buildApiResponse(result, httpStatus, "Get list general code successfully !") ;
-		}else {
+		HttpStatus httpStatus = HttpStatus.OK;
+		String resMessage = "Get list general code successfully";
+		if(CommonUtils.isEmptyData(result)) {
 			httpStatus = HttpStatus.INTERNAL_SERVER_ERROR;
-			dataResponse = ApiUtils.buildApiResponse(null, httpStatus, "Get list general code failed !") ;
+			resMessage = "Get list general code failed";
 		}
-
-		log.debug("[GENERAL-CODE-CONTROLLER] - getListGeneralByCommonCode :: {} !",dataResponse.message());
-		return new ResponseEntity<>(dataResponse, httpStatus);
+		ResponseEntity<ApiResponse<List<GeneralCodeResponseDTO>>> dataResponse =  ApiUtils.buildAPIResponse(result, httpStatus, resMessage);
+		log.debug("[GENERAL-CODE-CONTROLLER] - getListGeneralByCommonCode :: {} !",dataResponse.getBody().message());
+		return dataResponse;
 	}
 	
 }
